641f898bea66e6edc56c8144e8ab9e55362222e2
Gonzalo Gjuka <gonzalo.gjuka@hotmail.com>
2018-12-10 23:03:14 -0300

    Ejercicios JS

---
+++ test.js
@@ -0,0 +1,16 @@
+function fecha (){
+    var hoy=new Date();
+    document.write(hoy);
+}
+
+// declaracion
+
+function suma (num1,num2){
+    var resultado = num1+num2;
+}               
+
+// llamada
+
+suma(5, 7);
+
+function fecha (){var hoy=new Date(); document.write(hoy);}
